d01a91d32bd1d076aceac261ea35dfc3e1abe850
jieun <jieun_0330@naver.com>
2023-11-02 14:25:22 +0900

    first commit

---
+++ ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by 박지은 on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListTest01()
+        }
+    }
+}
---
+++ ListTest01.swift
@@ -0,0 +1,18 @@
+//
+//  ListTest01.swift
+//  List
+//
+//  Created by 박지은 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ListTest01: View {
+    var body: some View {
+        List {
+            Text("A List Item")
+            Text("A Second List Item")
+            Text("A Third List Item")
+        }
+    }
+}
